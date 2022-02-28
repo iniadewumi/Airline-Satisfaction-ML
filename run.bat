7e069d3c84d94cef9f0bf8b29102eaf2c55df420
iniadewumi <72786828+iniadewumi@users.noreply.github.com>
2022-02-27 18:53:22 -0600

    ..

--- run.bat
+++
@@ -1,4 +0,0 @@
-cd "C:\Users\adewu\OneDrive\Desktop\Python Files\Working Directory\Machine Learning\Airline-Satisfaction-ML"
-"C:\Users\adewu\anaconda3\python" "main.py"
-REM"C:\Users\adewu\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Anaconda3 (64-bit)\Spyder.lnk" "main.py"
-pause
--- vs_bash.bat
+++
@@ -1,2 +0,0 @@
-cd "C:\Users\adewu\OneDrive\Desktop\Python Files\Working Directory\Machine Learning\"
-code Airline-Satisfaction-ML
